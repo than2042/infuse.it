6b1660ba14f2a642512d0a8ca56079f312f6cd47
Fiona Kempton <fkempton38@gmail.com>
2024-02-16 16:54:10 +0000

    api calls and search page added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Link from "next/link";
 
 const HomePage = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <Link href={"/Search"}>Search</Link>
     </div>
   );
 };
