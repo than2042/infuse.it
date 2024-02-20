8b1dc26741cab3ce9a3b6b63d934344c69c135dd
than2042 <than.than2042@gmail.com>
2024-02-20 13:04:37 +0000

    fixed error

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,6 +1,10 @@
-default export function SearchInput = () => {
-    return (
-        <input className="px-5 py-1 w-2/3 sm:px5 sm:py3 flex-1 text-zinc-500 bg-lime-50"
-        placeholder="What are you in the mood for?" />
-    )
-}
+const SearchInput = () => {
+  return (
+    <input
+      className="px-5 py-1 w-2/3 sm:px5 sm:py3 flex-1 text-zinc-500 bg-lime-50"
+      placeholder="What are you in the mood for?"
+    />
+  );
+};
+
+export default SearchInput;
