2b296cb1a6fac0105ae3b1a4955da23ea9739e23
than2042 <than.than2042@gmail.com>
2024-02-21 21:59:43 +0000

    media-query on home, profile form

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import JoinToMix from "@/components/JoinToMix";
 
 const HomePage = () => {
   return (
-    <div className="m-auto flex flex-col justify-center w-11/12">
+    <div className="flex flex-col justify-center w-full">
       <HomeStatic />
       <JoinToMix />
     </div>
