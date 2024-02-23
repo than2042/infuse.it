edf0d6361d7a043e78b5e003178a91378c448dab
than2042 <than.than2042@gmail.com>
2024-02-23 09:54:41 +0000

    wip for check create userprofile

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,10 +4,10 @@ import { FormProvider } from "@/context/FormContext";
 import Header from "@/components/Header";
 import { ApiProvider } from "@/context/ApiContext";
 import { UserProvider } from "@/context/UserContext";
-import { db } from "@/db";
+// import CreateProfile from "@/components/CreateProfile";
+// import { db } from "@/db";
 
 import "./globals.css";
-// import CreateProfile from "@/components/CreateProfile";
 
 const montserratFont = Montserrat({ subsets: ["latin"] });
 
@@ -19,11 +19,11 @@ export const metadata = {
 export default async function RootLayout({ children }) {
   const { userId } = auth();
 
-  //   const profileCreate = await db.query(
-  //     `SELECT * FROM users WHERE clerk_user_id = $1`,
-  //     [userId]
-  //   );
-  //   console.log("profileCreate", profileCreate);
+  // const profileCreate = await db.query(
+  //   `SELECT * FROM users WHERE clerk_user_id = $1`,
+  //   [userId]
+  // );
+  // console.log("profileCreate", profileCreate);
 
   // const rowCount = profileCreate?.rowCount || 0;
   // console.log(rowCount);
@@ -33,7 +33,7 @@ export default async function RootLayout({ children }) {
         <ApiProvider>
           <UserProvider userId={userId}>
             <html lang="en">
-              <body>
+              <body className={montserratFont.className}>
                 <div className="flex justify-evenly m-auto">
                   {userId && <UserButton afterSignOutUrl="/" />}
                   <Header />
@@ -41,7 +41,7 @@ export default async function RootLayout({ children }) {
                 {/* {rowCount !== 0 && ( */}
                 <main className={montserratFont.className}>{children}</main>
                 {/* )} */}
-                {/* {userId && rowCount === 0 && <Link href={"/"}></Link>} */}
+                {/* {userId && rowCount === 0 && <CreateProfile />} */}
               </body>
             </html>
           </UserProvider>
